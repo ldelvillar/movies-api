d99c79306c1ba0712bc69f8349a028a30947024c
100495726 <ldelvillarvazquez@gmail.com>
2025-01-22 16:36:20 +0100

    add delete movie endpoint

--- app.js
+++ app.js
@@ -67,6 +67,19 @@ app.post('/movies', (req, res) => {
     res.status(201).json(newMovie);
 });
 
+app.delete('/movies/:id', (req, res) => {
+    const { id } = req.params;
+    const movieIndex = movies.findIndex(movie => movie.id === id);
+  
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' });
+    }
+  
+    movies.splice(movieIndex, 1);
+  
+    return res.json({ message: 'Movie deleted' });
+})
+
 app.patch('/movies/:id', (req, res) => {
     const result = validatePartialMovie(req.body);
 
