7814d28c91abba9141d8eeac55f4ad01dc7bafa1
ldelvillar <ldelvillarvazquez@gmail.com>
2025-04-30 16:21:07 +0200

    deploy database to google cloud

--- models/mysql/movie.js
+++ models/mysql/movie.js
@@ -1,12 +1,16 @@
 import mysql from 'mysql2/promise';
 import { validate as isUUID } from 'uuid';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const config = {
-    host: 'localhost',
-    user: 'root',
-    port: '3306',
-    password: '',
-    database: 'moviesdb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT || 3306,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 };
 
 const connection = await mysql.createConnection(config);
